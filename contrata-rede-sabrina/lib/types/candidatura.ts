b3a7da65f6f29d27b1ff1e64cbf5603cf71985c0
Rede Sabrina <bruno.redesabrina@gmail.com>
2026-09-04 10:13:09 -0300

    update: atualizado candidatura type
    criado um type para o status. Assim permitindo apenas aqueles status para a candidatura.

--- contrata-rede-sabrina/lib/types/candidatura.ts
+++ contrata-rede-sabrina/lib/types/candidatura.ts
@@ -1,7 +1,13 @@
+export type StatusCandidatura = 
+| 'Pendente'
+| 'Aprovado'
+| 'Reprovado'
+| 'Em analise';
+
 export interface Candidatura{
     id: number;
     candidato_id: number;
     vaga_id: number;
-    status: string;
+    status: StatusCandidatura;
     criado_em: string;
 }
